3d5aff32ad3226c1a0c8a771782656e5b297548f
lokadityahariharan <lokaditya18@gmail.com>
2025-03-12 12:11:30 -0400

    3/12/25

--- lharihar-lab3/static/script.js
+++ lharihar-lab3/static/script.js
@@ -1,8 +1,62 @@
 x = 7
 y = 5
 z = x+y
-print(z)
-a = "Hello"
-b = "World"
+console.log(z)
+a = "Hello "
+b = "World!"
 c = a+b
-print(c)
+console.log(c)
+function SumNPrint(x1,x2)
+{
+    x3 = x1+x2
+    console.log(x3)
+    return x3
+}
+SumNPrint(x,y)
+SumNPrint(a,b)
+if(c.length < z)
+{
+    console.log(z)
+}
+else if(c.length>z)
+{
+    console.log(c)
+}
+else
+{
+    console.log("good job")
+}
+L1 = ["Watermelon","Pineapple", "Pear", "Banana"]
+L2 = ["Apple","Banana", "Kiwi", "Orange"]
+function findTheBanana(s,index)
+{
+    if(s=="Banana"){
+        alert("Banana found at " + index)
+    }
+}
+L1.forEach(findTheBanana)
+L2.forEach(findTheBanana)
+function greetings()
+{
+    d = new Date
+    h = d.getHours()
+    if(window.location.href.indexOf("index.html") != -1)
+    {
+        if(h<12){
+            document.getElementById("Greeting").innerHTML = "Good Morning, I am Lok"  
+            }
+        else if(h<18){
+            document.getElementById("Greeting").innerHTML = "Good Afternoon, I am Lok"
+            }
+        else if(h<20)
+        {
+            document.getElementById("Greeting").innerHTML = "Good Evening, I am Lok"
+        }
+        else
+        {
+            document.getElementById("Greeting").innerHTML = "Good Night, I am Lok"
+        }
+    }
+    
+}
+greetings()
